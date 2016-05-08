52cb791794612912de8c8b2abd8365173cdf16fd
Firstianss <tiansatya@gmail.com>
2016-05-09 05:55:25 +0700

    155150200111230_Firstian Satya Y

---
+++ src/polimorfisme/Gold.java
@@ -0,0 +1,51 @@
+package polimorfisme;
+public class Gold extends Salon {
+    private String Produk,Layanan;
+    private int Dis, Harga, Jumlah, HargaP, HargaL;
+      public Gold (String p, String l, int J) {
+        this.Produk = p;
+        if (Produk.equalsIgnoreCase("Maskara")) {
+            HargaP = 50000;
+        } else if (Produk.equalsIgnoreCase("Lotion")) {
+            HargaP = 25000;
+        } else if (Produk.equalsIgnoreCase("HairStyle")) {
+            HargaP = 30000;
+        } else if (Produk.equalsIgnoreCase("Parfum")) {
+            HargaP = 105000;
+        } else {
+            HargaP = 0;
+        }
+        this.Layanan = l;
+        if (Layanan.equalsIgnoreCase("Potong")) {
+            HargaL = 12000;
+        } else if (Layanan.equalsIgnoreCase("Creambath")) {
+            HargaL = 15000;
+        } else if (Layanan.equalsIgnoreCase("Lulur")) {
+            HargaL = 35000;
+        } else if (Layanan.equalsIgnoreCase("Pijat++")) {
+            HargaL = 110000;
+        } else {
+            HargaL = 0;
+        }
+    }
+    public void setDiskon(int d) {
+        Dis = d;
+    }
+    public int getDiskon() {
+        return Dis;
+    }
+    public int DisProduk() {
+        int p = (HargaP*Dis/100)*85/ 100;
+        return p;
+    }
+    public int DisLayanan() {
+        int l = HargaL*85/ 100;
+        return l;
+    }
+    public int Total() {
+        Harga = (DisProduk() + DisLayanan());
+        return Harga;
+    }
+
+}
+
